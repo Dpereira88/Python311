4c0a83e0614664c7df669b60a9c4c7cca41df259
Dpereira88 <diogompereira3@gmail.com>
2022-12-11 14:08:44 +0000

    added new function insert_values_from_list
    this function add to the excel by given:
    - list of values where the list need to be list inside the list(matrix), ex: List_of_lists = [[Values]]
    - start line point
    - start column point

--- test1_excel.py
+++ test1_excel.py
@@ -20,7 +20,6 @@ def openWorkbook(xlapp, xlfile):
             xlwb = None                    
     return(xlwb)
 
-#get the line number of the last cell empty from one given column
 def get_empty_line(_cells, start_line, _column):
     # Initialize the value and count variables
     value = 0
@@ -31,7 +30,6 @@ def get_empty_line(_cells, start_line, _column):
         value = _cells(count, _column).Value
     # Return the line number of the first empty cell
     return count
-
 
 def clear_cells(_cells, start_line, end_line , start_column, end_column):
     # Increment the end coordinates by 1 to include the end cells in the range
@@ -49,22 +47,32 @@ def clear_cells(_cells, start_line, end_line , start_column, end_column):
         # Increment the column character by 1
         _column = chr(ord(_column) + 1)
 
+def insert_values_from_list(_cells, list_w_Values, start_line, start_column):
+    value = 0
+    count = 0
+    _line = start_line
+    for line_values in list_w_Values:
+        _column = start_column
+        for column_value in line_values:
+            _cells(_line,_column).Value = column_value
+            _column = chr(ord(_column) + 1)
+        _line += 1
+        
+        
 
 try:
     excel = win32.gencache.EnsureDispatch('Excel.Application')
-    wb = openWorkbook(excel, "Livro1.xlsx")
+    wb = openWorkbook(excel, "C:\GitHub\Python311\Livro1.xlsx")
     ws = wb.Worksheets('Sheet1') 
     excel.Visible = True
     cells = ws.Cells
     #write code here
-    
     #get the last line from a given column
     number_empty_line = get_empty_line(cells, 1, "A")
-    
     #clear cells(cells, start_line, end_line , start_column, end_column) 
-    clear_cells(cells, 1, 1, "A", "D")
-    
-    
+    #clear_cells(cells, 1, 1, "A", "D")
+    list_values = [[1,2,3,4,5,6,7,8],["A","B","C","D","E"]]
+    insert_values_from_list(cells, list_values, 4, "D")
 
 except Exception as e:
     print(e)
